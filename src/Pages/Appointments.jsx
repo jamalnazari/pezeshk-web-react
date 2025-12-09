517c30016761a6173dd668aaed6fb153d427c659
jamalnazari <jamalnazari15@gmail.com>
2025-12-09 22:38:40 +0330

    تغیرات جزِِیی

---
+++ src/Pages/Appointments.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Appointments = () => {
+  return (
+    <div className="container mt-4">
+      <h3>مدیریت نوبت‌ها</h3>
+      {/* فرم ثبت نوبت جدید */}
+      {/* جدول نوبت‌های ثبت‌شده */}
+    </div>
+  );
+};
+
+export default Appointments;
